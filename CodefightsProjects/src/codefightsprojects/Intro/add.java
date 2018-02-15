8ef495ab2b661e6c9b4fdb3000fd0d26e712c1c9
isiryk <siryk.ivan@gmail.com>
2018-02-15 12:57:36 -0500

    Codefights: Add

---
+++ CodefightsProjects/src/codefightsprojects/Intro/add.java
@@ -0,0 +1,14 @@
+
+package codefightsprojects.Intro;
+
+/**
+ * Task: Write a function that returns the sum of two numbers.
+ * @author Kepler
+ */
+
+
+public class add {
+    public int add(int param1, int param2) {
+         return param1 + param2;
+    }
+}
